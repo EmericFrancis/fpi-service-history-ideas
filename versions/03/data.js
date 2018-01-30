fdb8a1535ff3e80ccbc8c170786e22c9ccddffdc
Emeric Francis <Emeric.Francis@landregistry.gov.uk>
2018-01-30 09:07:12 +0000

    Changed tezt to be generic

--- versions/03/data.js
+++ versions/03/data.js
@@ -2,7 +2,7 @@ var data = {
 	"version": "Beta pass. December 2017",
 	"last-updated": "18 December 2017",
 	"userjourneys": [{
-			"title": "Get property summary information",
+			"title": "Bike reported to police",
 			"path": [{
 					"caption": "Start",
 					"imgref": "images/get-property-information/01-start-on-govuk.png",
@@ -12,12 +12,12 @@ var data = {
 					]
 				},
 				{
-					"caption": "Search",
+					"caption": "Reported",
 					"imgref": "images/get-property-information/02-search.png",
 					"note": "Postcode only."
 				},
 				{
-					"caption": "Select address",
+					"caption": "Crime Refrence",
 					"imgref": "images/get-property-information/03-results.png",
 					"note": [
 						"All the addresses in a postcode.",
@@ -25,96 +25,31 @@ var data = {
 						"Places with postal addresses, not 'land at'",
 						"more examples of results and property page below"
 					]
-				},
-				{
-					"caption": "Select property record (title)",
-					"imgref": "images/get-property-information/04-property-page.png",
-					"note": [
-						"The property titles for an address *",
-						"* all that we can currently link to this address (percent?)",
-						"first version of this page",
-						"more examples of results and property page below"
-					]
-
-				},
-				{
-					"caption": "Property title",
-					"imgref": "images/get-property-information/05-about-property-title.png",
-					"note": "What you'll get if you continue"
-
-				},
-				{
-					"caption": "Tell us who you are",
-					"imgref": "images/get-property-information/06b-sign-in.png",
-					"note": [
-						"Create an account or sign-in",
-						"Create account more likely - 70% to 30%",
-						"more account function shown below"
-					]
-				},
-				{
-					"caption": "Confirm property title wanted",
-					"imgref": "images/get-property-information/07-confirm-purchase.png",
-					"note": [
-						"What you're getting when you pay",
-						"Title number given here for free"
-					]
-				},
-				{
-					"caption": "Payment - part 1",
-					"imgref": "images/get-property-information/08-worldpay-1.png",
-					"note": "Select card"
-				},
-				{
-					"caption": "Payment - part 2",
-					"imgref": "images/get-property-information/08-worldpay-2.png",
-					"note": "card details"
-				},
-				{
-					"caption": "Payment - part 3",
-					"imgref": "images/get-property-information/08-worldpay-3.png",
-					"note": "Confirmation"
-				},
-				{
-					"caption": "Summary details",
-					"imgref": "images/get-property-information/09-summary.png",
-					"note": [
-						"PDF version to 'take away'",
-						"can view this for an hour, but cannot reaccess this again without paying"
-					]
-				},
-				{
-					"caption": "What next?",
-					"imgref": "images/get-property-information/10-signout.png",
-					"note": [
-						"Sign out is one thing you can do after viewing the summary"
-					]
 				}
-
 			]
 		},
 		{
-			"title": "Variations of search results",
+			"title": "Bike not reported to the police at location",
 			"path": [{
-					"caption": "Pagination",
+					"caption": "Start",
 					"imgref": "images/results/01-pagination.png",
 					"note": [
 						"20 results per page (max 5 pages)"
 					]
 				},
 				{
-					"caption": "Not much available here",
+					"caption": "Current Location",
 					"imgref": "images/results/02-not-much-available.png"
 				},
 				{
-					"caption": "Why nothing available?",
+					"caption": "Geo Search Results",
 					"imgref": "images/results/03-why-no-information-available.png",
 					"note": [
 						"Most likely reason is we cannot yet link to all the property records per address"
 					]
 				},
 				{
-					"caption": "Sorting can be hard",
+					"caption": "Contact Detials",
 					"imgref": "images/results/04-sorting.png",
 					"note": [
 						"Sorting into a logical order is hard.  Because - AlphaNumeric numbers, and building names and business names"
